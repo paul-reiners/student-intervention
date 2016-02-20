28095b2beb1a16804047a7d9b8b151f7344116f8
Paul Reiners <paul.reiners@gmail.com>
2016-02-20 12:53:27 -0600

    Adding table.

---
+++ list_table.py
@@ -0,0 +1,22 @@
+'''
+@author: Caleb Madrigal
+From http://calebmadrigal.com/display-list-as-table-in-ipython-notebook/
+'''
+
+class ListTable(list):
+    """ Overridden list class which takes a 2-dimensional list of 
+        the form [[1,2,3],[4,5,6]], and renders an HTML Table in 
+        IPython Notebook. """
+    
+    def _repr_html_(self):
+        html = ["<table>"]
+        for row in self:
+            html.append("<tr>")
+            
+            for col in row:
+                html.append("<td>{0}</td>".format(col))
+            
+            html.append("</tr>")
+        html.append("</table>")
+        return ''.join(html)
+    
